80889cfa82cb4e71c44e9545af9286c0327dfd89
Pablo Rebolledo <pablo.rebolledo0@walmart.com>
2024-05-24 16:44:23 -0400

    reset project

--- apps/project-one/src/domain/ports/users.ts
+++
@@ -1,5 +0,0 @@
-import { User } from "../entities/user";
-
-export interface UsersPort {
-  add: (user: User) => Promise<string>
-};
--- apps/project-one/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { makeAddUserUseCase } from "./use-cases/add-user";
-
-const init = async () => {
-  const addUserUseCase = makeAddUserUseCase();
-  const user = await addUserUseCase({name: "pablo", email: "pablo@gmai.com"});
-  console.log({user});
-};
-
-init();
